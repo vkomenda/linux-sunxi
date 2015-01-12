e8781ee342fc92da99450d4c880d9bfe9e030d9e
Vladimir Komendantskiy <vladimir@hutchison-t.com>
2015-01-12 17:41:29 +0000

    mtd: nand: hynix: chip patch

--- drivers/mtd/nand/nand_hynix.c
+++ drivers/mtd/nand/nand_hynix.c
@@ -16,29 +16,37 @@
 #include <linux/mtd/nand.h>
 #include <linux/slab.h>
 
+#define DBG(fmt, arg...) pr_info(pr_fmt("%s: " fmt "\n"), __FUNCTION__, ##arg)
+
 static u8 h27ucg8t2a_read_retry_regs[] = {
 	0xcc, 0xbf, 0xaa, 0xab, 0xcd, 0xad, 0xae, 0xaf
 };
 
+static u8 h27ucg8t2e_read_retry_regs[] = {
+	0x38, 0x39, 0x3a, 0x3b
+};
+
 struct hynix_read_retry {
-	u8 *regs;
-	u8 values[64];
+	u8 regnum;      // number of registers to set on each RR step
+	u8 *regs;       // array of register addresses
+	u8 values[64];  // RR values to be written into the RR registers
 };
 
 struct hynix_nand {
 	struct hynix_read_retry read_retry;
 };
 
-int nand_setup_read_retry_hynix(struct mtd_info *mtd, int retry_mode)
+int nand_setup_read_retry_hynix(struct mtd_info *mtd, int retry_count)
 {
 	struct nand_chip *chip = mtd->priv;
 	struct hynix_nand *hynix = chip->manuf_priv;
-	int offset = retry_mode * 8;
+	int offset = retry_count * hynix->read_retry.regnum;
 	int status;
 	int i;
 
+	DBG("%d", retry_count);
 	chip->cmdfunc(mtd, 0x36, -1, -1);
-	for (i = 0; i < 8; i++) {
+	for (i = 0; i < hynix->read_retry.regnum; i++) {
 		int column = hynix->read_retry.regs[i];
 		column |= column << 8;
 		chip->cmdfunc(mtd, NAND_CMD_NONE, column, -1);
@@ -118,6 +126,114 @@ static int h27ucg8t2a_init(struct mtd_info *mtd, const uint8_t *id)
 	return ret;
 }
 
+static void h27ucg8t2e_cleanup(struct mtd_info *mtd)
+{
+	struct nand_chip *chip = mtd->priv;
+	kfree(chip->manuf_priv);
+}
+
+#define UCG8T2E_RRT_OTP_SIZE 528
+
+static int h27ucg8t2e_init(struct mtd_info *mtd, const uint8_t *id)
+{
+	struct nand_chip *chip = mtd->priv;
+	struct hynix_nand *hynix;
+	u8 rrtOTP[UCG8T2E_RRT_OTP_SIZE];
+	int rrtReg, rrtSet, i;
+	int ret;
+
+	DBG("mtd %p, ID %.2x %.2x %.2x %.2x %.2x %.2x",
+	    mtd, id[0], id[1], id[2], id[3], id[4], id[5]);
+
+	chip->select_chip(mtd, 0);
+	chip->cmdfunc(mtd, NAND_CMD_RESET, -1, -1);
+
+	/* copy RRT from OTP to rrtOTP */
+	chip->cmdfunc(mtd, 0x36, 0x38, -1);
+	chip->write_byte(mtd, 0x52);
+	chip->cmdfunc(mtd, 0x16, -1, -1);
+	chip->cmdfunc(mtd, 0x17, -1, -1);
+	chip->cmdfunc(mtd, 0x04, -1, -1);
+	chip->cmdfunc(mtd, 0x19, -1, -1);
+	chip->cmdfunc(mtd, NAND_CMD_READ0, 0, 0x200);
+
+	/*
+	 *  1. Read total RR count - 1 byte x 8 times - and RR register count -
+	 *  also 1 byte x 8 times.
+	 *
+	 *  2. Read 8 RR register sets, each consisting of a 32-byte original
+	 *  and its 32-byte inverse copy. Every set stores 8 lists of values for
+	 *  the 4 RR registers on the flash chip.
+	 */
+	chip->read_buf(mtd, rrtOTP, UCG8T2E_RRT_OTP_SIZE);
+	printk("RR count (8 copies), RR reg. count (8 copies):\n");
+	for (i = 0; i < 16; i++) {
+		printk(" %.2x", rrtOTP[i]);
+	}
+
+	if (rrtOTP[0] != 8 || rrtOTP[1] != 8) {
+		DBG("wrong total RR count %u (%u)", rrtOTP[0], rrtOTP[1]);
+		return -EINVAL;
+	}
+
+//	chip->read_buf(mtd, rrtOTP, 512);
+
+	/* copy RRT from OTP, command suffix */
+	chip->cmdfunc(mtd, NAND_CMD_RESET, -1, -1);
+	chip->cmdfunc(mtd, 0x36, 0x38, -1);
+	chip->write_byte(mtd, 0);
+
+	/* dummy read from any address */
+	chip->cmdfunc(mtd, NAND_CMD_READ0, 0, 0);
+
+	chip->select_chip(mtd, -1);
+
+	/* FIXME: common function - majority check, not "all correct" */
+	ret = 0;
+	printk("RRT sets in OTP: original (inverse)...\n");
+	for (rrtSet = 0; rrtSet < 8; rrtSet++) {
+		u8 *cur = rrtOTP + 16 + (64 * rrtSet);
+		for (rrtReg = 0; rrtReg < 32; rrtReg++) {
+			uint8_t original = cur[rrtReg];
+			uint8_t inverse  = cur[rrtReg + 32];
+			printk(" %.2x (%.2x)", original, inverse);
+			if ((original | inverse) != 0xff) {
+				ret = -EINVAL;
+				break;
+			}
+		}
+		printk("\n");
+
+		if (ret)
+			// read the next set
+			continue;
+		else
+			// current set is correct
+			break;
+	}
+
+	if (ret) {
+		DBG("Loading of RRT from OTP failed. Using defaults.");
+
+		hynix = kzalloc(sizeof(*hynix), GFP_KERNEL);
+		if (!hynix)
+			return -ENOMEM;
+
+		hynix->read_retry.regs = h27ucg8t2e_read_retry_regs;
+		hynix->read_retry.regnum = 4;
+
+		// copy the first correct RRT set (the original half)
+		memcpy(hynix->read_retry.values, &rrtOTP[rrtSet * 64], 32);
+
+		chip->manuf_priv = hynix;
+		chip->setup_read_retry = nand_setup_read_retry_hynix;
+		chip->read_retries = 8;
+		chip->manuf_cleanup = h27ucg8t2e_cleanup;
+	}
+
+	return ret;
+}
+
 struct hynix_nand_initializer {
 	u8 id[6];
 	int (*init)(struct mtd_info *mtd, const uint8_t *id);
@@ -127,6 +243,10 @@ struct hynix_nand_initializer initializers[] = {
 	{
 		.id = {NAND_MFR_HYNIX, 0xde, 0x94, 0xda, 0x74, 0xc4},
 		.init = h27ucg8t2a_init,
+	},
+	{
+		.id = {NAND_MFR_HYNIX, 0xde, 0x14, 0xa7, 0x42, 0x4a},
+		.init = h27ucg8t2e_init,
 	},
 };
 
--- drivers/mtd/nand/nand_ids.c
+++ drivers/mtd/nand/nand_ids.c
@@ -56,6 +56,9 @@ struct nand_flash_dev nand_flash_ids[] = {
 	{"K9GBG08U0B 32G 3.3V 8-bit",
 		{ .id = {0xec, 0xd7, 0x94, 0x7e, 0x64, 0x44} },
 		  SZ_8K, SZ_4K, SZ_1M, 0, 6, 1024, NAND_ECC_INFO(40, SZ_1K) },
+	{"H27UCG8T2ETR-BC 8GiB 3.3V 8-bit",
+		{ .id = {0xad, 0xde, 0x14, 0xa7, 0x42, 0x4a} },
+		  SZ_16K, SZ_8K, SZ_4M, 0, 6, 1664, NAND_ECC_INFO(40, SZ_1K) },
 
 	LEGACY_ID_NAND("NAND 4MiB 5V 8-bit",   0x6B, 4, SZ_8K, SP_OPTIONS),
 	LEGACY_ID_NAND("NAND 4MiB 3,3V 8-bit", 0xE3, 4, SZ_8K, SP_OPTIONS),
